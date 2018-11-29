75b5654b41bfef011f7b620c26bb82ae7b9f413c
Aigodis <mariann.szucs92@gmail.com>
2018-11-29 13:48:22 +0100

    Updates

---
+++ precache-manifest.a18b467d7b019cfb4f42bdbd6c453782.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1aeaa1666d5c82f11bf2b28ccfc64470",
+    "url": "/starwarsapi/static/media/bckgr.1aeaa166.jpg"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/starwarsapi/static/media/Starjedi.cc3019aa.ttf"
+  },
+  {
+    "revision": "37d37c9a3806a951dd35",
+    "url": "/starwarsapi/static/js/runtime~main.37d37c9a.js"
+  },
+  {
+    "revision": "d777d033d28053709ed1",
+    "url": "/starwarsapi/static/js/main.d777d033.chunk.js"
+  },
+  {
+    "revision": "ae97301e364d4f2ef4d8",
+    "url": "/starwarsapi/static/js/1.ae97301e.chunk.js"
+  },
+  {
+    "revision": "d777d033d28053709ed1",
+    "url": "/starwarsapi/static/css/main.829af165.chunk.css"
+  },
+  {
+    "revision": "ae97301e364d4f2ef4d8",
+    "url": "/starwarsapi/static/css/1.587e7d97.chunk.css"
+  },
+  {
+    "revision": "c005dff14697e78cd3ebdabc1e708778",
+    "url": "/starwarsapi/index.html"
+  }
+];
